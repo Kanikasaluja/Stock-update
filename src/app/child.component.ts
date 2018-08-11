8abe17bd73c0b56c81595ea885d7368a8a1463b0
kanikasaluja <kanikasaluja1551@gmail.com>
2018-08-11 14:07:24 +0000

    updatedpackage

--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-child',
-  template: `<h2>Child123</h2>`,
+  template: `<h2>Child123n</h2>`,
 })
 export class ChildComponent {
   title = 'stock-update';
