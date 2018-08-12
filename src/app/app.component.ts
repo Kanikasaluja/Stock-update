8e3d1b604cdb8df6875f56b0886884c2da82ce4b
kanikasaluja <kanikasaluja1551@gmail.com>
2018-08-12 18:22:37 +0000

    stockupdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'stock-update';
+  title = 'stock-updater' ;
+  product1 = 700;
+  product2 = 800;
+  product3 = 900;
+  // x = document.getElementById(inputbox);
+   update() {
+    if (this.product1 > 0) {
+      this.product1 = this.product1 - 100;
+}}
+ update1() {
+  if (this.product2 > 0) {
+    this.product2 = this.product2 - 100;
+}}
+ update2() {
+  if (this.product3 > 0) {
+    this.product3 = this.product3 - 100;
+}
+}
 }
--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output} from '@angular/core';
+
+
 
 @Component({
   selector: 'app-child',
-  template: `<h2>Child123nnpm </h2>`,
+  template: `<div>
+  
+  <input type="text" class="form-control">
+
+<button type="submit" (click)="childfunction()">Update</button> </div>
+`,
 })
 export class ChildComponent {
-  title = 'stock-update';
+  title = 'stock-updater';
+  @Output() myEvent = new EventEmitter();
+
+  childfunction() {
+      this.myEvent.emit();
+  }
+
 }
